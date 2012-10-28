758a4688d1155aad12a54530f34423bec9d3db5d
alex <alex@cf7a9c80-4257-4e8c-b9e2-f0888625b119>
2012-10-28 17:15:25 +0000

    Alter Pfad gelöscht.
    git-svn-id: file:///home/nik/repositories/svn/master@90 cf7a9c80-4257-4e8c-b9e2-f0888625b119

--- tq1/projects/MetaMaster/project/trunk/src/model/meta/JuristicPersonMssgs.java
+++
@@ -1,8 +0,0 @@
-package model.meta;
-
-public interface JuristicPersonMssgs {
-    void accept(JuristicPersonMssgsVisitor visitor) throws persistence.PersistenceException;
-}
-
-interface JuristicPersonDOWNMssgs extends ActorDOWNMssgs, JuristicPersonMssgs{}
-interface JuristicPersonUPMssgs extends Mssgs, JuristicPersonMssgs{}
--- tq1/projects/MetaMaster/project/trunk/src/model/meta/PositionMssgs.java
+++
@@ -1,8 +0,0 @@
-package model.meta;
-
-public interface PositionMssgs {
-    void accept(PositionMssgsVisitor visitor) throws persistence.PersistenceException;
-}
-
-interface PositionDOWNMssgs extends Mssgs, PositionMssgs{}
-interface PositionUPMssgs extends Mssgs, PositionMssgs{}
