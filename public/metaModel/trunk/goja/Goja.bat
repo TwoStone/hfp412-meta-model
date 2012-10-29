9a2c39e6d4d25d44f803fe12f6749c904f09f828
alex <alex@cf7a9c80-4257-4e8c-b9e2-f0888625b119>
2012-10-29 08:58:26 +0000

    Aktuelle Goja-Version eingefügt (inkl. Bat-Datei zum Starten unter Windows).
    git-svn-id: file:///home/nik/repositories/svn/master@99 cf7a9c80-4257-4e8c-b9e2-f0888625b119

---
+++ public/metaModel/trunk/goja/Goja.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set currentGojaFile="Goja20121027aJVM6.jar"
+
+set java7BinDir="C:\Program Files (x86)\Java\jre7\bin"
+
+start "Goja" /B /D "%~f0\.." %java7BinDir%\javaw.exe -jar %currentGojaFile%
