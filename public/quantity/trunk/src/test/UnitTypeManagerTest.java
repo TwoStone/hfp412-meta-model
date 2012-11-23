09522886b8938c98d744983ebd90e7848d8e5e30
stefan <stefan@cf7a9c80-4257-4e8c-b9e2-f0888625b119>
2012-11-23 18:05:46 +0000

    2 neue Testfälle für das Anlegen von CompoundUnitTypes
    git-svn-id: file:///home/nik/repositories/svn/master@167 cf7a9c80-4257-4e8c-b9e2-f0888625b119

--- public/quantity/trunk/src/test/UnitTypeManagerTest.java
+++ public/quantity/trunk/src/test/UnitTypeManagerTest.java
@@ -5,14 +5,20 @@ package test;
 
 import junit.framework.TestCase;
 import model.AbsUnitType;
+import model.AlreadyFinalizedException;
 import model.DoubleDefinitionException;
 import model.UnitTypeManager;
+import model.visitor.BooleanValueReturnVisitor;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import persistence.PersistenceException;
+import persistence.PersistentBooleanFalse;
+import persistence.PersistentBooleanTrue;
+import persistence.PersistentBooleanValue;
+import persistence.PersistentCompUnitType;
 import persistence.PersistentUnitType;
 import persistence.PersistentUnitTypeManager;
 import constants.ExceptionConstants;
@@ -117,6 +123,91 @@ public class UnitTypeManagerTest extends TestCase{
 	@Test
 	public void testAddDefaultUnit(){
 		//TODO!!
+	}
+	/**
+	 * Testet das Erstellen und abschlieﬂen einer Compound Unit
+	 * Nachbedingungen: Name gesetzt, |Refs| = 0, isFinal = false
+	 */
+	@Test
+	public void testCreateCompoundUnitType(){
+		try {
+			PersistentUnitTypeManager utm = UnitTypeManager.getTheUnitTypeManager();
+			String name = "Hello, I'm a complex Type!";
+			try {
+				utm.createCompUnitType(name);
+				PersistentCompUnitType cut = (PersistentCompUnitType)AbsUnitType.getAbsUnitTypeByName(name).iterator().next();
+				assertEquals(name, cut.getName());
+				assertTrue(cut.getRefs().getLength() == 0);
+				assertFalse(this.isTrue(cut.isFinal()));
+			} catch (DoubleDefinitionException e) {
+				fail("Es sollte keine DoubleDefinitionException geben.");
+			}
+			
+		} catch (PersistenceException e) {
+			fail("Exception: " + e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testCreateCompoundUnitTypeSpeedExample(){
+		try {
+			PersistentUnitTypeManager utm = UnitTypeManager.getTheUnitTypeManager();
+			String nameOfLength = "Length";
+			String nameOfTime = "Time";
+			String nameOfSpeed = "Speed";
+			try {
+				utm.createUnitType(nameOfLength);
+				utm.createUnitType(nameOfTime);
+				utm.createCompUnitType(nameOfSpeed);
+				PersistentUnitType length = (PersistentUnitType) AbsUnitType.getAbsUnitTypeByName(nameOfLength).iterator().next();
+				PersistentUnitType time = (PersistentUnitType) AbsUnitType.getAbsUnitTypeByName(nameOfTime).iterator().next();
+				PersistentCompUnitType speed = (PersistentCompUnitType)AbsUnitType.getAbsUnitTypeByName(nameOfSpeed).iterator().next();
+				assertFalse(this.isTrue(speed.isFinal()));
+				try {
+					utm.addReferenceType(speed, length, 1);
+					utm.addReferenceType(speed, time, -1);
+					assertFalse(this.isTrue(speed.isFinal()));
+					speed.finalize();
+				} catch (AlreadyFinalizedException e) {
+					fail("AlreadyFinalizedException vor finalize()-Aufruf!");
+				}
+				assertTrue(this.isTrue(speed.isFinal()));
+				
+			} catch (DoubleDefinitionException e) {
+				fail("Es sollte keine DoubleDefinitionException geben.");
+			}
+		} catch (PersistenceException e) {
+			fail("Exception: " + e.getMessage());
+		}
+		
+	}
+
+	/**
+	 * Macht aus BooleanValue ein Java-Boolean (Visitor Aufruf ausgelagert);-)
+	 * weil Instanceof ist uncool
+	 * @throws PersistenceException 
+	 * 
+	 */
+	private boolean isTrue(PersistentBooleanValue bool) throws PersistenceException {
+		boolean result = false;
+		result = bool.accept(new BooleanValueReturnVisitor< Boolean >() {
+
+			@Override
+			public Boolean handleBooleanFalse(
+					PersistentBooleanFalse booleanFalse)
+					throws PersistenceException {
+				return false;
+			}
+
+			@Override
+			public Boolean handleBooleanTrue(PersistentBooleanTrue booleanTrue)
+					throws PersistenceException {
+				return true;
+			}
+
+			
+		});
+		return result;
 	}
 
 }
