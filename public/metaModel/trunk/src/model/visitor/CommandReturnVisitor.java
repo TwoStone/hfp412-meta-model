5fb4df7ca45fbd9efc21ef4b6b59e41917e000ee
niklas <niklas@cf7a9c80-4257-4e8c-b9e2-f0888625b119>
2013-02-02 16:35:03 +0000

    Visitor excludiert
    git-svn-id: file:///home/nik/repositories/svn/master@280 cf7a9c80-4257-4e8c-b9e2-f0888625b119

--- public/metaModel/trunk/src/model/visitor/CommandReturnVisitor.java
+++
@@ -1,7 +0,0 @@
-
-package model.visitor;
-
-public interface CommandReturnVisitor<R> extends AssociationManagerCommandReturnVisitor<R> ,AccountTypeManagerCommandReturnVisitor<R> ,TypeManagerCommandReturnVisitor<R> ,AspectManagerCommandReturnVisitor<R> ,MessageManagerCommandReturnVisitor<R> ,MeasurementTypeManagerCommandReturnVisitor<R> ,NameSchemeManagerCommandReturnVisitor<R> ,QuantityManagerCommandReturnVisitor<R> ,AccountManagerCommandReturnVisitor<R> ,ObjectManagerCommandReturnVisitor<R> ,UnitTypeManagerCommandReturnVisitor<R> ,LinkManagerCommandReturnVisitor<R> ,OperationManagerCommandReturnVisitor<R> {
-    
-    
-}
