5bf18a3de5209304eaead6091d7bc6a2a28e1226
christin <christin@cf7a9c80-4257-4e8c-b9e2-f0888625b119>
2013-02-03 13:18:55 +0000

    createMeassage: Exception-Tests
    git-svn-id: file:///home/nik/repositories/svn/master@288 cf7a9c80-4257-4e8c-b9e2-f0888625b119

--- public/metaModel/trunk/src/test/MessageManagerTest.java
+++ public/metaModel/trunk/src/test/MessageManagerTest.java
@@ -137,4 +137,127 @@ public class MessageManagerTest extends AbstractTest {
 			}
 		}).getActualParameters().getLength());
 	}
+
+	@Test(expected = ConsistencyException.class)
+	public void createStandardMessageFailureWrongSource() throws PersistenceException, DoubleDefinitionException,
+			ConsistencyException {
+		ActualParameterSearchList ap = new ActualParameterSearchList();
+		assertEquals(0, manager.getMessages().getLength());
+		manager.createMessage(standardOp, mao6, mao6, ap);
+		assertEquals(0, manager.getMessages().getLength());
+	}
+
+	@Test(expected = ConsistencyException.class)
+	public void createStandardMessageFailureWrongTarget() throws PersistenceException, DoubleDefinitionException,
+			ConsistencyException {
+		ActualParameterSearchList ap = new ActualParameterSearchList();
+		assertEquals(0, manager.getMessages().getLength());
+		manager.createMessage(standardOp, mao1, mao1, ap);
+		assertEquals(0, manager.getMessages().getLength());
+	}
+
+	@Test(expected = ConsistencyException.class)
+	public void createVoidMessageFailureWrongSource() throws PersistenceException, DoubleDefinitionException,
+			ConsistencyException {
+		ActualParameterSearchList ap = new ActualParameterSearchList();
+		assertEquals(0, manager.getMessages().getLength());
+		manager.createVoidMessage(voidOp, mao6, ap);
+		assertEquals(0, manager.getMessages().getLength());
+	}
+
+	@Test(expected = ConsistencyException.class)
+	public void createConstantFailureWrongTarget() throws PersistenceException, DoubleDefinitionException,
+			ConsistencyException {
+		assertEquals(0, manager.getMessages().getLength());
+		manager.createConst(constantOp, "constantOperation", mao6);
+		assertEquals(0, manager.getMessages().getLength());
+	}
+
+	@Test(expected = ConsistencyException.class)
+	public void createStaticMessageFailureWrongTarget() throws PersistenceException, DoubleDefinitionException,
+			ConsistencyException {
+		ActualParameterSearchList ap = new ActualParameterSearchList();
+		assertEquals(0, manager.getMessages().getLength());
+		manager.createStaticMessage(staticOp, "staticOperation", mao6, ap);
+		assertEquals(0, manager.getMessages().getLength());
+	}
+
+	@Test(expected = ConsistencyException.class)
+	public void createStandardMessageFailureWrongType() throws PersistenceException, DoubleDefinitionException,
+			ConsistencyException {
+		ActualParameterSearchList ap = new ActualParameterSearchList();
+		assertEquals(0, manager.getMessages().getLength());
+		manager.createMessage(voidOp, mao6, mao1, ap);
+		assertEquals(0, manager.getMessages().getLength());
+	}
+
+	@Test(expected = ConsistencyException.class)
+	public void createVoidMessageFailureWrongType() throws PersistenceException, DoubleDefinitionException,
+			ConsistencyException {
+		ActualParameterSearchList ap = new ActualParameterSearchList();
+		assertEquals(0, manager.getMessages().getLength());
+		manager.createVoidMessage(standardOp, mao6, ap);
+		assertEquals(0, manager.getMessages().getLength());
+	}
+
+	@Test(expected = ConsistencyException.class)
+	public void createConstantFailureWrongType() throws PersistenceException, DoubleDefinitionException,
+			ConsistencyException {
+		assertEquals(0, manager.getMessages().getLength());
+		manager.createConst(standardOp, "constantOperation", mao6);
+		assertEquals(0, manager.getMessages().getLength());
+	}
+
+	@Test(expected = ConsistencyException.class)
+	public void createStaticMessageFailureWrongType() throws PersistenceException, DoubleDefinitionException,
+			ConsistencyException {
+		ActualParameterSearchList ap = new ActualParameterSearchList();
+		assertEquals(0, manager.getMessages().getLength());
+		manager.createStaticMessage(standardOp, "staticOperation", mao6, ap);
+		assertEquals(0, manager.getMessages().getLength());
+	}
+
+	@Test(expected = DoubleDefinitionException.class)
+	public void createStandardMessageFailureDDE() throws PersistenceException, DoubleDefinitionException,
+			ConsistencyException {
+		ActualParameterSearchList ap = new ActualParameterSearchList();
+		assertEquals(0, manager.getMessages().getLength());
+		manager.createMessage(standardOp, mao1, mao6, ap);
+		assertEquals(1, manager.getMessages().getLength());
+		manager.createMessage(standardOp, mao1, mao6, ap);
+		assertEquals(1, manager.getMessages().getLength());
+	}
+
+	@Test
+	public void createVoidMessageFailureDDE() throws PersistenceException, DoubleDefinitionException,
+			ConsistencyException {
+		ActualParameterSearchList ap = new ActualParameterSearchList();
+		assertEquals(0, manager.getMessages().getLength());
+		manager.createVoidMessage(voidOp, mao1, ap);
+		assertEquals(1, manager.getMessages().getLength());
+		manager.createVoidMessage(voidOp, mao1, ap);
+		assertEquals(1, manager.getMessages().getLength());
+	}
+
+	@Test
+	public void createConstantMessageFailureDDE() throws PersistenceException, DoubleDefinitionException,
+			ConsistencyException {
+		ActualParameterSearchList ap = new ActualParameterSearchList();
+		assertEquals(0, manager.getMessages().getLength());
+		manager.createConst(constantOp, "constantOperation", mao1);
+		assertEquals(1, manager.getMessages().getLength());
+		manager.createConst(constantOp, "constantOperation", mao1);
+		assertEquals(1, manager.getMessages().getLength());
+	}
+
+	@Test
+	public void createStaticMessageFailureDDE() throws PersistenceException, DoubleDefinitionException,
+			ConsistencyException {
+		ActualParameterSearchList ap = new ActualParameterSearchList();
+		assertEquals(0, manager.getMessages().getLength());
+		manager.createStaticMessage(staticOp, "staticOperation", mao1, ap);
+		assertEquals(1, manager.getMessages().getLength());
+		manager.createStaticMessage(staticOp, "staticOperation", mao1, ap);
+		assertEquals(1, manager.getMessages().getLength());
+	}
 }
