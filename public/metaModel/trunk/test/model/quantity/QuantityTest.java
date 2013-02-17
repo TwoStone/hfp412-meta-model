feca78bca58e1d92c820904bb35bc52bf75064df
stefan <stefan@cf7a9c80-4257-4e8c-b9e2-f0888625b119>
2013-02-17 14:09:43 +0000

    Quantity Test Case Klasse
    git-svn-id: file:///home/nik/repositories/svn/master@326 cf7a9c80-4257-4e8c-b9e2-f0888625b119

---
+++ public/metaModel/trunk/test/model/quantity/QuantityTest.java
@@ -0,0 +1,29 @@
+package model.quantity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class QuantityTest {
+
+	@Test
+	public void testSub() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMul() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDiv() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+}
