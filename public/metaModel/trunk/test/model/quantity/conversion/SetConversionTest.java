8d4e0825155f3dff629ab19cffabcd4b203e5b73
malte <malte@cf7a9c80-4257-4e8c-b9e2-f0888625b119>
2013-02-25 20:00:24 +0000

    Testfälle für das Ändern einer Default-Unit hinzugefügt
    git-svn-id: file:///home/nik/repositories/svn/master@369 cf7a9c80-4257-4e8c-b9e2-f0888625b119

--- public/metaModel/trunk/test/model/quantity/conversion/SetConversionTest.java
+++ public/metaModel/trunk/test/model/quantity/conversion/SetConversionTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 import persistence.PersistenceException;
 import persistence.PersistentUnit;
 import persistence.PersistentUnitType;
+import persistence.PersistentUnitTypeManager;
 import util.TestingBase;
 
 import common.Fraction;
@@ -23,23 +24,83 @@ public class SetConversionTest extends TestingBase {
 	}
 
 	@Test(expected = ConsistencyException.class)
-	public void setConversionWithoutDefaultUnit() throws PersistenceException, ConsistencyException {
+	public void testSetConversionWithoutDefaultUnit() throws PersistenceException, ConsistencyException {
+		PersistentUnitTypeManager utm = this.getManager(UnitTypeManager.class);
 		PersistentUnitType unitTypeGewicht = UnitType.createUnitType("Gewicht");
 		PersistentUnit unitKg = Unit.createUnit(unitTypeGewicht, "Kg");
 
-		UnitTypeManager.getTheUnitTypeManager().setConversion(unitKg, Fraction.parse("1"), Fraction.parse("0"));
+		utm.setConversion(unitKg, Fraction.parse("1"), Fraction.parse("0")); // Fehler
 	}
 
-	public void setConversionTest() throws PersistenceException, ConsistencyException {
+	@Test
+	public void testSetDefaultConversion() throws PersistenceException, ConsistencyException {
+		PersistentUnitTypeManager utm = this.getManager(UnitTypeManager.class);
+
 		PersistentUnitType unitTypeGewicht = UnitType.createUnitType("Gewicht");
 		PersistentUnit unitKg = Unit.createUnit(unitTypeGewicht, "Kg");
-		unitTypeGewicht.setDefaultUnit(unitKg);
+		utm.setDefaultUnit(unitTypeGewicht, unitKg);
 
-		UnitTypeManager.getTheUnitTypeManager().setConversion(unitKg, Fraction.parse("1"), Fraction.parse("0"));
+		// UnitTypeManager.getTheUnitTypeManager().setConversion(unitKg, Fraction.parse("1"), Fraction.parse("0"));
+
+		Assert.assertEquals("Conversion weist einen falschen Factor auf", Fraction.parse("1"), unitKg.getMyConversion()
+				.getMyFunction().getFactor());
+		Assert.assertEquals("Conversion weist eine falsche Konstante auf", Fraction.parse("0"), unitKg
+				.getMyConversion().getMyFunction().getConstant());
+	}
+
+	@Test
+	public void testChangeDefaultUnit() throws PersistenceException, ConsistencyException {
+		PersistentUnitTypeManager utm = this.getManager(UnitTypeManager.class);
+
+		PersistentUnitType unitTypeGewicht = UnitType.createUnitType("Gewicht");
+		PersistentUnit unitKg = Unit.createUnit(unitTypeGewicht, "Kg");
+		PersistentUnit unitG = Unit.createUnit(unitTypeGewicht, "g");
+
+		// Default: g
+		// Conversion: 1g = 1g
+		utm.setDefaultUnit(unitTypeGewicht, unitG);
+
+		// Conversion für kg (1g = 1/1000g)
+		utm.setConversion(unitKg, Fraction.parse("1/1000"), Fraction.parse("0"));
+
+		Assert.assertEquals("Conversion weist einen falschen Factor auf", Fraction.parse("1/1000"), unitKg
+				.getMyConversion().getMyFunction().getFactor());
+
+		// Default: kg
+		utm.setDefaultUnit(unitTypeGewicht, unitKg);
+
+		// Conversions wurden angepasst?
+		Assert.assertEquals("Conversion weist einen falschen Factor auf", Fraction.parse("1"), unitKg.getMyConversion()
+				.getMyFunction().getFactor());
+
+		// Wurde die g-Conversion auf 1kg = 1000g gesetzt?
+		Assert.assertEquals("Conversion weist einen falschen Factor auf", Fraction.parse("1000"), unitG
+				.getMyConversion().getMyFunction().getFactor());
+
+	}
+
+	@Test
+	public void testChangeDefaultUnitToConversionlessUnit() throws PersistenceException, ConsistencyException {
+		PersistentUnitTypeManager utm = this.getManager(UnitTypeManager.class);
+
+		PersistentUnitType unitTypeGewicht = UnitType.createUnitType("Gewicht");
+		PersistentUnit unitT = Unit.createUnit(unitTypeGewicht, "T");
+		PersistentUnit unitKg = Unit.createUnit(unitTypeGewicht, "Kg");
+		PersistentUnit unitG = Unit.createUnit(unitTypeGewicht, "g");
+		utm.setDefaultUnit(unitTypeGewicht, unitKg);
+
+		UnitTypeManager.getTheUnitTypeManager().setConversion(unitG, Fraction.parse("1000"), Fraction.parse("0"));
 
 		Assert.assertEquals("Conversion weist einen falschen Factor auf", unitKg.getMyConversion().getMyFunction()
 				.getFactor(), Fraction.parse("1"));
-		Assert.assertEquals("Conversion weist eine falsche Konstante auf", unitKg.getMyConversion().getMyFunction()
-				.getConstant(), Fraction.parse("0"));
+
+		Assert.assertEquals("Conversion weist einen falschen Factor auf", unitG.getMyConversion().getMyFunction()
+				.getFactor(), Fraction.parse("1000"));
+
+		utm.setDefaultUnit(unitTypeGewicht, unitT); // Conversionless Unit
+
+		Assert.assertEquals("Conversion weist einen falschen Factor auf", null, unitKg.getMyConversion());
+		Assert.assertEquals("Conversion weist einen falschen Factor auf", null, unitG.getMyConversion());
+
 	}
 }
