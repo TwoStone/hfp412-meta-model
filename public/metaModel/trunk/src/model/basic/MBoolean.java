4f3b3b505511896ffaef3b5f164b0feff203ccb5
marius <marius@cf7a9c80-4257-4e8c-b9e2-f0888625b119>
2013-03-01 16:01:03 +0000

    Todos removed
    git-svn-id: file:///home/nik/repositories/svn/master@384 cf7a9c80-4257-4e8c-b9e2-f0888625b119

--- public/metaModel/trunk/src/model/basic/MBoolean.java
+++ public/metaModel/trunk/src/model/basic/MBoolean.java
@@ -11,115 +11,121 @@ import persistence.TDObserver;
 
 /* Additional import section end */
 
-public abstract class MBoolean extends PersistentObject implements PersistentMBoolean{
-    
-    /** Throws persistence exception if the object with the given id does not exist. */
-    public static PersistentMBoolean getById(long objectId) throws PersistenceException{
-        long classId = ConnectionHandler.getTheConnectionHandler().theMBooleanFacade.getClass(objectId);
-        return (PersistentMBoolean)PersistentProxi.createProxi(objectId, classId);
-    }
-    
-    public java.util.Hashtable<String,Object> toHashtable(java.util.Hashtable<String,Object> allResults, int depth, int essentialLevel, boolean forGUI, boolean leaf, TDObserver tdObserver) throws PersistenceException {
-    java.util.Hashtable<String,Object> result = null;
-        if (depth > 0 && essentialLevel <= common.RPCConstantsAndServices.EssentialDepth){
-            result = super.toHashtable(allResults, depth, essentialLevel, forGUI, false, tdObserver);
-            String uniqueKey = common.RPCConstantsAndServices.createHashtableKey(this.getClassId(), this.getId());
-            if (leaf && !allResults.contains(uniqueKey)) allResults.put(uniqueKey, result);
-        }
-        return result;
-    }
-    
-    public abstract MBoolean provideCopy() throws PersistenceException;
-    
-    public boolean hasEssentialFields() throws PersistenceException{
-        return false;
-    }
-    protected PersistentMBoolean This;
-    
-    public MBoolean(PersistentMBoolean This,long id) throws persistence.PersistenceException {
-        /* Shall not be used by clients for object construction! Use static create operation instead! */
-        super(id);
-        if (This != null && !(this.equals(This))) this.This = This;        
-    }
-    
-    static public long getTypeId() {
-        return 134;
-    }
-    
-    public long getClassId() {
-        return getTypeId();
-    }
-    
-    public void store() throws PersistenceException {
-        if(!this.isDelayed$Persistence()) return;
-        super.store();
-        if(!this.equals(this.getThis())){
-            this.getThis().store();
-            ConnectionHandler.getTheConnectionHandler().theMBooleanFacade.ThisSet(this.getId(), getThis());
-        }
-        
-    }
-    
-    protected void setThis(PersistentMBoolean newValue) throws PersistenceException {
-        if (newValue == null) throw new PersistenceException("Null values not allowed!", 0);
-        if (newValue.equals(this)){
-            this.This = null;
-            return;
-        }
-        if(newValue.equals(this.This)) return;
-        long objectId = newValue.getId();
-        long classId = newValue.getClassId();
-        this.This = (PersistentMBoolean)PersistentProxi.createProxi(objectId, classId);
-        if(!this.isDelayed$Persistence()){
-            newValue.store();
-            ConnectionHandler.getTheConnectionHandler().theMBooleanFacade.ThisSet(this.getId(), newValue);
-        }
-    }
-    public abstract PersistentMBoolean getThis() throws PersistenceException ;
-    
-    
-    
-    public void initialize(final Anything This, final java.util.Hashtable<String,Object> final$$Fields) 
-				throws PersistenceException{
-        this.setThis((PersistentMBoolean)This);
-		if(this.equals(This)){
+public abstract class MBoolean extends PersistentObject implements PersistentMBoolean {
+
+	/** Throws persistence exception if the object with the given id does not exist. */
+	public static PersistentMBoolean getById(final long objectId) throws PersistenceException {
+		final long classId = ConnectionHandler.getTheConnectionHandler().theMBooleanFacade.getClass(objectId);
+		return (PersistentMBoolean) PersistentProxi.createProxi(objectId, classId);
+	}
+
+	@Override
+	public java.util.Hashtable<String, Object> toHashtable(final java.util.Hashtable<String, Object> allResults,
+			final int depth, final int essentialLevel, final boolean forGUI, final boolean leaf,
+			final TDObserver tdObserver) throws PersistenceException {
+		java.util.Hashtable<String, Object> result = null;
+		if (depth > 0 && essentialLevel <= common.RPCConstantsAndServices.EssentialDepth) {
+			result = super.toHashtable(allResults, depth, essentialLevel, forGUI, false, tdObserver);
+			final String uniqueKey = common.RPCConstantsAndServices.createHashtableKey(this.getClassId(), this.getId());
+			if (leaf && !allResults.contains(uniqueKey))
+				allResults.put(uniqueKey, result);
 		}
-    }
-    
-    
-    // Start of section that contains operations that must be implemented.
-    
-    public void copyingPrivateUserAttributes(final Anything copy) 
-				throws PersistenceException{
-        //TODO: implement method: copyingPrivateUserAttributes
-        
-    }
-    public void initializeOnCreation() 
-				throws PersistenceException{
-		// TODO: implement method: initializeOnCreation
+		return result;
+	}
+
+	@Override
+	public abstract MBoolean provideCopy() throws PersistenceException;
+
+	@Override
+	public boolean hasEssentialFields() throws PersistenceException {
+		return false;
+	}
+
+	protected PersistentMBoolean This;
+
+	public MBoolean(final PersistentMBoolean This, final long id) throws persistence.PersistenceException {
+		/* Shall not be used by clients for object construction! Use static create operation instead! */
+		super(id);
+		if (This != null && !(this.equals(This)))
+			this.This = This;
+	}
+
+	static public long getTypeId() {
+		return 134;
+	}
+
+	@Override
+	public long getClassId() {
+		return getTypeId();
+	}
+
+	@Override
+	public void store() throws PersistenceException {
+		if (!this.isDelayed$Persistence())
+			return;
+		super.store();
+		if (!this.equals(this.getThis())) {
+			this.getThis().store();
+			ConnectionHandler.getTheConnectionHandler().theMBooleanFacade.ThisSet(this.getId(), getThis());
+		}
 
 	}
-    public void initializeOnInstantiation() 
-				throws PersistenceException{
-		// TODO: implement method: initializeOnInstantiation
 
+	protected void setThis(final PersistentMBoolean newValue) throws PersistenceException {
+		if (newValue == null)
+			throw new PersistenceException("Null values not allowed!", 0);
+		if (newValue.equals(this)) {
+			this.This = null;
+			return;
+		}
+		if (newValue.equals(this.This))
+			return;
+		final long objectId = newValue.getId();
+		final long classId = newValue.getClassId();
+		this.This = (PersistentMBoolean) PersistentProxi.createProxi(objectId, classId);
+		if (!this.isDelayed$Persistence()) {
+			newValue.store();
+			ConnectionHandler.getTheConnectionHandler().theMBooleanFacade.ThisSet(this.getId(), newValue);
+		}
 	}
-    
-    
-    // Start of section that contains overridden operations only.
-    
 
-    /* Start of protected part that is not overridden by persistence generator */
-    
-	public static PersistentMBoolean createFromBoolean(boolean bool) throws PersistenceException {
+	@Override
+	public abstract PersistentMBoolean getThis() throws PersistenceException;
+
+	@Override
+	public void initialize(final Anything This, final java.util.Hashtable<String, Object> final$$Fields)
+			throws PersistenceException {
+		this.setThis((PersistentMBoolean) This);
+		if (this.equals(This)) {
+		}
+	}
+
+	// Start of section that contains operations that must be implemented.
+
+	@Override
+	public void copyingPrivateUserAttributes(final Anything copy) throws PersistenceException {
+	}
+
+	@Override
+	public void initializeOnCreation() throws PersistenceException {
+	}
+
+	@Override
+	public void initializeOnInstantiation() throws PersistenceException {
+	}
+
+	// Start of section that contains overridden operations only.
+
+	/* Start of protected part that is not overridden by persistence generator */
+
+	public static PersistentMBoolean createFromBoolean(final boolean bool) throws PersistenceException {
 		return bool ? MTrue.getTheMTrue() : MFalse.getTheMFalse();
 	}
 
-	public static PersistentMBoolean createFromFactoryString(String factoryString) throws PersistenceException {
+	public static PersistentMBoolean createFromFactoryString(final String factoryString) throws PersistenceException {
 		return StringFACTORY.createObjectBySubTypeNameForMBoolean(factoryString);
 	}
 
-	
-    /* End of protected part that is not overridden by persistence generator */
-    
+	/* End of protected part that is not overridden by persistence generator */
+
 }
--- public/metaModel/trunk/src/model/basic/MFalse.java
+++ public/metaModel/trunk/src/model/basic/MFalse.java
@@ -1,4 +1,3 @@
-
 package model.basic;
 
 import model.UserException;
@@ -18,168 +17,201 @@ import persistence.PersistentMBoolean;
 import persistence.PersistentMFalse;
 import persistence.TDObserver;
 
-
 /* Additional import section end */
 
-public class MFalse extends model.basic.MBoolean implements PersistentMFalse{
-    
-    private static PersistentMFalse theMFalse = null;
-    public static boolean reset$For$Test = false;
-    private static final Object $$lock = new Object();
-    public static PersistentMFalse getTheMFalse() throws PersistenceException{
-        if (theMFalse == null || reset$For$Test){
-            class Initializer implements Runnable {
-                PersistenceException exception = null;
-                public void run(){
-                    try {
-                        MFalseProxi proxi = null;
-                        synchronized ($$lock){
-                            proxi = ConnectionHandler.getTheConnectionHandler().theMFalseFacade.getTheMFalse();
-                            theMFalse = proxi;
-                        }
-                        if(proxi.getId() < 0) {
-                            proxi.setId(proxi.getId() * -1);
-                            proxi.initialize(proxi, new java.util.Hashtable<String,Object>());
-                            proxi.initializeOnCreation();
-                        }
-                    } catch (PersistenceException e){
-                        exception = e;
-                    }
-                    synchronized ($$lock){$$lock.notify();}
-                }
-                PersistentMFalse getResult() throws PersistenceException{
-                    if(exception != null) throw exception;
-                    return theMFalse;
-                }
-            }
-            synchronized ($$lock) {
-                reset$For$Test = false;
-                Initializer initializer = new Initializer();
-                new Thread(initializer).start();
-                try {$$lock.wait();}catch (InterruptedException e) {} //Need not to be interrupted
-                return initializer.getResult();
-            }
-        }
-        return theMFalse;
-    }
-    public java.util.Hashtable<String,Object> toHashtable(java.util.Hashtable<String,Object> allResults, int depth, int essentialLevel, boolean forGUI, boolean leaf, TDObserver tdObserver) throws PersistenceException {
-    java.util.Hashtable<String,Object> result = null;
-        if (depth > 0 && essentialLevel <= common.RPCConstantsAndServices.EssentialDepth){
-            result = super.toHashtable(allResults, depth, essentialLevel, forGUI, false, tdObserver);
-            String uniqueKey = common.RPCConstantsAndServices.createHashtableKey(this.getClassId(), this.getId());
-            if (leaf && !allResults.contains(uniqueKey)) allResults.put(uniqueKey, result);
-        }
-        return result;
-    }
-    
-    public MFalse provideCopy() throws PersistenceException{
-        MFalse result = this;
-        result = new MFalse(this.This, 
-                            this.getId());
-        this.copyingPrivateUserAttributes(result);
-        return result;
-    }
-    
-    public boolean hasEssentialFields() throws PersistenceException{
-        return false;
-    }
-    
-    public MFalse(PersistentMBoolean This,long id) throws persistence.PersistenceException {
-        /* Shall not be used by clients for object construction! Use static create operation instead! */
-        super((PersistentMBoolean)This,id);        
-    }
-    
-    static public long getTypeId() {
-        return 185;
-    }
-    
-    public long getClassId() {
-        return getTypeId();
-    }
-    
-    public void store() throws PersistenceException {
-        // Singletons cannot be delayed!
-    }
-    
-    public PersistentMFalse getThis() throws PersistenceException {
-        if(this.This == null){
-            PersistentMFalse result = new MFalseProxi(this.getId());
-            result.getTheObject();
-            return result;
-        }return (PersistentMFalse)this.This;
-    }
-    
-    public void accept(MBooleanVisitor visitor) throws PersistenceException {
-        visitor.handleMFalse(this);
-    }
-    public <R> R accept(MBooleanReturnVisitor<R>  visitor) throws PersistenceException {
-         return visitor.handleMFalse(this);
-    }
-    public <E extends UserException>  void accept(MBooleanExceptionVisitor<E> visitor) throws PersistenceException, E {
-         visitor.handleMFalse(this);
-    }
-    public <R, E extends UserException> R accept(MBooleanReturnExceptionVisitor<R, E>  visitor) throws PersistenceException, E {
-         return visitor.handleMFalse(this);
-    }
-    public void accept(AnythingVisitor visitor) throws PersistenceException {
-        visitor.handleMFalse(this);
-    }
-    public <R> R accept(AnythingReturnVisitor<R>  visitor) throws PersistenceException {
-         return visitor.handleMFalse(this);
-    }
-    public <E extends UserException>  void accept(AnythingExceptionVisitor<E> visitor) throws PersistenceException, E {
-         visitor.handleMFalse(this);
-    }
-    public <R, E extends UserException> R accept(AnythingReturnExceptionVisitor<R, E>  visitor) throws PersistenceException, E {
-         return visitor.handleMFalse(this);
-    }
-    public int getLeafInfo() throws PersistenceException{
-        return 0;
-    }
-    
-    
-    public void initialize(final Anything This, final java.util.Hashtable<String,Object> final$$Fields) 
-				throws PersistenceException{
-        this.setThis((PersistentMFalse)This);
-		if(this.equals(This)){
+public class MFalse extends model.basic.MBoolean implements PersistentMFalse {
+
+	private static PersistentMFalse theMFalse = null;
+	public static boolean reset$For$Test = false;
+	private static final Object $$lock = new Object();
+
+	public static PersistentMFalse getTheMFalse() throws PersistenceException {
+		if (theMFalse == null || reset$For$Test) {
+			class Initializer implements Runnable {
+				PersistenceException exception = null;
+
+				@Override
+				public void run() {
+					try {
+						MFalseProxi proxi = null;
+						synchronized ($$lock) {
+							proxi = ConnectionHandler.getTheConnectionHandler().theMFalseFacade.getTheMFalse();
+							theMFalse = proxi;
+						}
+						if (proxi.getId() < 0) {
+							proxi.setId(proxi.getId() * -1);
+							proxi.initialize(proxi, new java.util.Hashtable<String, Object>());
+							proxi.initializeOnCreation();
+						}
+					} catch (final PersistenceException e) {
+						exception = e;
+					}
+					synchronized ($$lock) {
+						$$lock.notify();
+					}
+				}
+
+				PersistentMFalse getResult() throws PersistenceException {
+					if (exception != null)
+						throw exception;
+					return theMFalse;
+				}
+			}
+			synchronized ($$lock) {
+				reset$For$Test = false;
+				final Initializer initializer = new Initializer();
+				new Thread(initializer).start();
+				try {
+					$$lock.wait();
+				} catch (final InterruptedException e) {
+				} // Need not to be interrupted
+				return initializer.getResult();
+			}
 		}
-    }
-    
-    
-    // Start of section that contains operations that must be implemented.
-    
-    public void copyingPrivateUserAttributes(final Anything copy) 
-				throws PersistenceException{
-        //TODO: implement method: copyingPrivateUserAttributes
-        
-    }
-    public void initializeOnCreation() 
-				throws PersistenceException{
-        //TODO: implement method: initializeOnCreation
-        
-    }
-    public void initializeOnInstantiation() 
-				throws PersistenceException{
-        //TODO: implement method: initializeOnInstantiation
-        
-    }
-    
-    
-    // Start of section that contains overridden operations only.
-    
-    public PersistentMBoolean invert() 
-				throws PersistenceException{
-		return MTrue.getTheMTrue();
+		return theMFalse;
 	}
-    public boolean toBoolean() 
-				throws PersistenceException{
+
+	@Override
+	public java.util.Hashtable<String, Object> toHashtable(final java.util.Hashtable<String, Object> allResults,
+			final int depth, final int essentialLevel, final boolean forGUI, final boolean leaf,
+			final TDObserver tdObserver) throws PersistenceException {
+		java.util.Hashtable<String, Object> result = null;
+		if (depth > 0 && essentialLevel <= common.RPCConstantsAndServices.EssentialDepth) {
+			result = super.toHashtable(allResults, depth, essentialLevel, forGUI, false, tdObserver);
+			final String uniqueKey = common.RPCConstantsAndServices.createHashtableKey(this.getClassId(), this.getId());
+			if (leaf && !allResults.contains(uniqueKey))
+				allResults.put(uniqueKey, result);
+		}
+		return result;
+	}
+
+	@Override
+	public MFalse provideCopy() throws PersistenceException {
+		MFalse result = this;
+		result = new MFalse(this.This, this.getId());
+		this.copyingPrivateUserAttributes(result);
+		return result;
+	}
+
+	@Override
+	public boolean hasEssentialFields() throws PersistenceException {
 		return false;
 	}
 
-    /* Start of protected part that is not overridden by persistence generator */
-    
-    
-    
-    /* End of protected part that is not overridden by persistence generator */
-    
+	public MFalse(final PersistentMBoolean This, final long id) throws persistence.PersistenceException {
+		/* Shall not be used by clients for object construction! Use static create operation instead! */
+		super(This, id);
+	}
+
+	static public long getTypeId() {
+		return 185;
+	}
+
+	@Override
+	public long getClassId() {
+		return getTypeId();
+	}
+
+	@Override
+	public void store() throws PersistenceException {
+		// Singletons cannot be delayed!
+	}
+
+	@Override
+	public PersistentMFalse getThis() throws PersistenceException {
+		if (this.This == null) {
+			final PersistentMFalse result = new MFalseProxi(this.getId());
+			result.getTheObject();
+			return result;
+		}
+		return (PersistentMFalse) this.This;
+	}
+
+	@Override
+	public void accept(final MBooleanVisitor visitor) throws PersistenceException {
+		visitor.handleMFalse(this);
+	}
+
+	@Override
+	public <R> R accept(final MBooleanReturnVisitor<R> visitor) throws PersistenceException {
+		return visitor.handleMFalse(this);
+	}
+
+	@Override
+	public <E extends UserException> void accept(final MBooleanExceptionVisitor<E> visitor)
+			throws PersistenceException, E {
+		visitor.handleMFalse(this);
+	}
+
+	@Override
+	public <R, E extends UserException> R accept(final MBooleanReturnExceptionVisitor<R, E> visitor)
+			throws PersistenceException, E {
+		return visitor.handleMFalse(this);
+	}
+
+	@Override
+	public void accept(final AnythingVisitor visitor) throws PersistenceException {
+		visitor.handleMFalse(this);
+	}
+
+	@Override
+	public <R> R accept(final AnythingReturnVisitor<R> visitor) throws PersistenceException {
+		return visitor.handleMFalse(this);
+	}
+
+	@Override
+	public <E extends UserException> void accept(final AnythingExceptionVisitor<E> visitor)
+			throws PersistenceException, E {
+		visitor.handleMFalse(this);
+	}
+
+	@Override
+	public <R, E extends UserException> R accept(final AnythingReturnExceptionVisitor<R, E> visitor)
+			throws PersistenceException, E {
+		return visitor.handleMFalse(this);
+	}
+
+	@Override
+	public int getLeafInfo() throws PersistenceException {
+		return 0;
+	}
+
+	@Override
+	public void initialize(final Anything This, final java.util.Hashtable<String, Object> final$$Fields)
+			throws PersistenceException {
+		this.setThis((PersistentMFalse) This);
+		if (this.equals(This)) {
+		}
+	}
+
+	// Start of section that contains operations that must be implemented.
+
+	@Override
+	public void copyingPrivateUserAttributes(final Anything copy) throws PersistenceException {
+	}
+
+	@Override
+	public void initializeOnCreation() throws PersistenceException {
+	}
+
+	@Override
+	public void initializeOnInstantiation() throws PersistenceException {
+	}
+
+	// Start of section that contains overridden operations only.
+
+	@Override
+	public PersistentMBoolean invert() throws PersistenceException {
+		return MTrue.getTheMTrue();
+	}
+
+	@Override
+	public boolean toBoolean() throws PersistenceException {
+		return false;
+	}
+
+	/* Start of protected part that is not overridden by persistence generator */
+
+	/* End of protected part that is not overridden by persistence generator */
+
 }
