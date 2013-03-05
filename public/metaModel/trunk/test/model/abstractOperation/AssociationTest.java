cc022f676b7fdd7e4ed33264871f510ed8d2b09b
christin <christin@cf7a9c80-4257-4e8c-b9e2-f0888625b119>
2013-03-05 19:39:14 +0000

    Testfälle isObservation erweitert
    git-svn-id: file:///home/nik/repositories/svn/master@421 cf7a9c80-4257-4e8c-b9e2-f0888625b119

--- public/metaModel/trunk/test/model/abstractOperation/AssociationTest.java
+++ public/metaModel/trunk/test/model/abstractOperation/AssociationTest.java
@@ -24,6 +24,9 @@ public class AssociationTest extends AbstractTest {
 		assertEquals(mFalse, Association.createAssociation("", mat1, mstSingle1).isObservation());
 		assertEquals(mTrue, Association.createAssociation("", mat1, mptEmpty).isObservation());
 		assertEquals(mTrue, Association.createAssociation("", mat1, mstEmpty).isObservation());
-		// TODO: Christin CompTypes testen
+		assertEquals(mFalse, Association.createAssociation("", mat1, mstMixed2And4Or5And6).isObservation());
+		assertEquals(mFalse, Association.createAssociation("", mat1, mptMixed2Or4And5Or6).isObservation());
+		assertEquals(mFalse, Association.createAssociation("", mstMixed2And4Or5And6, mstEmpty).isObservation());
+		assertEquals(mFalse, Association.createAssociation("", mptMixed2Or4And5Or6, mstEmpty).isObservation());
 	}
 }
