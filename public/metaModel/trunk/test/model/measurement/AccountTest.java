f975ebabb9b193131c0e0516064656fd0c38aa0a
bjoern <bjoern@cf7a9c80-4257-4e8c-b9e2-f0888625b119>
2013-03-08 21:22:51 +0000

    AccountTest: added some more testcases
    git-svn-id: file:///home/nik/repositories/svn/master@472 cf7a9c80-4257-4e8c-b9e2-f0888625b119

--- public/metaModel/trunk/test/model/measurement/AccountTest.java
+++ public/metaModel/trunk/test/model/measurement/AccountTest.java
@@ -131,7 +131,71 @@ public class AccountTest extends TestingBase {
 
 		try {
 			a1.addSubAccount(a2);
-			fail("Muss funktionieren, da accType2 <= accType1!");
+			fail("Darf nicht funktionieren, da accType3 NICHT<= accType1!");
+		} catch (final ConsistencyException e) {
+			// Exception korrekt!
+			assertEquals(ExceptionConstants.WRONG_ACCOUNT_TYPE, e.getMessage());
+		}
+	}
+
+	@Test
+	public void onAccount_test05() throws PersistenceException, CycleException {
+
+		final PersistentMAspect aspect1 = MAspect.createMAspect("bla");
+		final PersistentMAtomicType type1 = MAtomicType.createMAtomicType("A", mFalse, mFalse, aspect1);
+		final PersistentUnitType unitType1 = UnitType.createUnitType("Zeit");
+
+		final PersistentMAccountType accType1 = MAccountType.createMAccountType(type1, unitType1);
+		final PersistentMAccountType accType2 = MAccountType.createMAccountType(type1, unitType1);
+		final PersistentMAccountType accType3 = MAccountType.createMAccountType(type1, unitType1);
+		final PersistentMAccountType accType4 = MAccountType.createMAccountType(type1, unitType1);
+		accType1.addSubAccountType(accType2);
+		accType2.addSubAccountType(accType3);
+		accType2.addSubAccountType(accType4);
+
+		final PersistentMObject obj1 = MObject.createMObject();
+		final PersistentMObject obj2 = MObject.createMObject();
+		final PersistentMObject obj3 = MObject.createMObject();
+
+		final PersistentAccount a1 = Account.createAccount(obj1, accType1);
+		final PersistentAccount a2 = Account.createAccount(obj2, accType3);
+		final PersistentAccount a3 = Account.createAccount(obj2, accType4);
+
+		try {
+			a1.addSubAccount(a2);
+			a1.addSubAccount(a3);
+		} catch (final ConsistencyException e) {
+			// Exception falsch!
+			fail("Muss funktionieren, da accType3 und accType4<=accType2 und accType2<=accType1!");
+		}
+	}
+
+	@Test
+	public void onAccount_test06() throws PersistenceException, CycleException {
+
+		final PersistentMAspect aspect1 = MAspect.createMAspect("bla");
+		final PersistentMAtomicType type1 = MAtomicType.createMAtomicType("A", mFalse, mFalse, aspect1);
+		final PersistentUnitType unitType1 = UnitType.createUnitType("Zeit");
+
+		final PersistentMAccountType accType1 = MAccountType.createMAccountType(type1, unitType1);
+		final PersistentMAccountType accType2 = MAccountType.createMAccountType(type1, unitType1);
+		final PersistentMAccountType accType3 = MAccountType.createMAccountType(type1, unitType1);
+		final PersistentMAccountType accType4 = MAccountType.createMAccountType(type1, unitType1);
+		final PersistentMAccountType accType5 = MAccountType.createMAccountType(type1, unitType1);
+		accType1.addSubAccountType(accType2);
+		accType1.addSubAccountType(accType3);
+		accType3.addSubAccountType(accType4);
+		accType3.addSubAccountType(accType5);
+
+		final PersistentMObject obj1 = MObject.createMObject();
+		final PersistentMObject obj2 = MObject.createMObject();
+
+		final PersistentAccount a1 = Account.createAccount(obj1, accType2);
+		final PersistentAccount a2 = Account.createAccount(obj2, accType5);
+
+		try {
+			a1.addSubAccount(a2);
+			fail("Darf nicht funktionieren, da accType5 NICHT<= accType2!");
 		} catch (final ConsistencyException e) {
 			// Exception korrekt!
 			assertEquals(ExceptionConstants.WRONG_ACCOUNT_TYPE, e.getMessage());
