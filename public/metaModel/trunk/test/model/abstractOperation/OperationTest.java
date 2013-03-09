cf239f2307b185516eaa52f4bb49951cb1197f01
christin <christin@cf7a9c80-4257-4e8c-b9e2-f0888625b119>
2013-03-09 11:33:44 +0000

    OperationTests vervollständigt
    git-svn-id: file:///home/nik/repositories/svn/master@479 cf7a9c80-4257-4e8c-b9e2-f0888625b119

--- public/metaModel/trunk/test/model/abstractOperation/OperationTest.java
+++ public/metaModel/trunk/test/model/abstractOperation/OperationTest.java
@@ -1,10 +1,12 @@
 package model.abstractOperation;
 
 import static org.junit.Assert.assertEquals;
+import model.ConsistencyException;
 import model.DoubleDefinitionException;
 
 import org.junit.Test;
 
+import persistence.FormalParameterSearchList;
 import persistence.PersistenceException;
 import persistence.PersistentOperationManager;
 import util.AbstractTest;
@@ -16,14 +18,33 @@ public class OperationTest extends AbstractTest {
 	private PersistentOperationManager manager;
 
 	@Test
-	public void isStatic() throws PersistenceException, DoubleDefinitionException {
-		assertEquals(mFalse, Operation.createOperation("", mat4, mat1).isStatic());
-		assertEquals(mFalse, Operation.createOperation("", mptMultiple2And4, mat1).isStatic());
-		assertEquals(mFalse, Operation.createOperation("", mptSingle1, mat1).isStatic());
-		assertEquals(mFalse, Operation.createOperation("", mstMultiple2And4, mat1).isStatic());
-		assertEquals(mFalse, Operation.createOperation("", mstSingle1, mat1).isStatic());
-		assertEquals(mFalse, Operation.createOperation("", mptEmpty, mat1).isStatic());
-		assertEquals(mTrue, Operation.createOperation("", mstEmpty, mat1).isStatic());
-		// TODO: Christin CompTypes testen
+	public void isStatic() throws PersistenceException, DoubleDefinitionException, ConsistencyException {
+		assertEquals(mFalse, manager.createOperation(mat4, mat1, "a", new FormalParameterSearchList()).isStatic());
+		assertEquals(mFalse, manager.createOperation(mptMultiple2And4, mat1, "b", new FormalParameterSearchList())
+				.isStatic());
+		assertEquals(mFalse, manager.createOperation(mptSingle1, mat1, "c", new FormalParameterSearchList()).isStatic());
+		assertEquals(mFalse, manager.createOperation(mstMultiple2And4, mat1, "d", new FormalParameterSearchList())
+				.isStatic());
+		assertEquals(mFalse, manager.createOperation(mstSingle1, mat1, "e", new FormalParameterSearchList()).isStatic());
+		assertEquals(mFalse, manager.createOperation(mptEmpty, mat1, "f", new FormalParameterSearchList()).isStatic());
+		assertEquals(mTrue, manager.createOperation(mstEmpty, mat1, "g", new FormalParameterSearchList()).isStatic());
+	}
+
+	@Test
+	public void isConstant() throws PersistenceException, DoubleDefinitionException, ConsistencyException {
+		assertEquals(mFalse, manager.createOperation(mat4, mat1, "a", new FormalParameterSearchList()).isConstant());
+		assertEquals(mFalse, manager.createOperation(mptMultiple2And4, mat1, "b", new FormalParameterSearchList())
+				.isConstant());
+		assertEquals(mFalse, manager.createOperation(mptSingle1, mat1, "c", new FormalParameterSearchList())
+				.isConstant());
+		assertEquals(mFalse, manager.createOperation(mstMultiple2And4, mat1, "d", new FormalParameterSearchList())
+				.isConstant());
+		assertEquals(mFalse, manager.createOperation(mstSingle1, mat1, "e", new FormalParameterSearchList())
+				.isConstant());
+		assertEquals(mFalse, manager.createOperation(mptEmpty, mat1, "f", new FormalParameterSearchList()).isConstant());
+		assertEquals(mTrue, manager.createOperation(mstEmpty, mat1, "g", new FormalParameterSearchList()).isConstant());
+		final FormalParameterSearchList fpList = new FormalParameterSearchList();
+		fpList.add(manager.createFp("a", mat2));
+		assertEquals(mFalse, manager.createOperation(mstEmpty, mat1, "h", fpList).isConstant());
 	}
 }
